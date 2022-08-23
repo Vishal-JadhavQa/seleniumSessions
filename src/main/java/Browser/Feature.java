05dd99d1db8da047660e6992a8b490d77c10ec21
vishal-camplus <vishal.jadhav@camplus.co.in>
2022-08-23 07:17:40 +0530

    feature

---
+++ src/main/java/Browser/Feature.java
@@ -0,0 +1,20 @@
+package Browser;
+
+public class Feature {
+
+	public void f2() {
+		System.out.println("f2 feature");
+	}
+	
+	public void f3() {
+		System.out.println("f3 feature");
+	}
+	
+	public void f4() {
+		System.out.println("f4 feature");
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
