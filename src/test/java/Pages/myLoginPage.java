2b161fc89cb5f9ccee9abfdece239a7c538ba394
Vishal-JadhavQa <110076099+Vishal-JadhavQa@users.noreply.github.com>
2022-08-23 19:37:37 +0530

    Update myLoginPage.java

--- src/test/java/Pages/myLoginPage.java
+++ src/test/java/Pages/myLoginPage.java
@@ -3,7 +3,7 @@ package Pages;
 public class myLoginPage {
 
 	public void login() {
-		System.out.println("vishal - login");
-		
+		System.out.println("riya - login");
+		String name = "Riya";
 	}
 }
